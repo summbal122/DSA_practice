6a37bf24352e1fece1f47650f70c329fe5f0d617
summbal122 <sunbalijaz13@gmail.com>
2025-09-04 10:22:49 +0500

    Add code

---
+++ recursion/fibonacciNumber.js
@@ -0,0 +1,11 @@
+//recursive approach
+function fibonacciNumber(n){
+  if( n<= 1) return n
+  return fibonacciNumber(n-1) + fibonacciNumber(n-2)
+
+}
+console.log(fibonacciNumber(5))
+
+// two approaches
+// iterative approach
+// recursive approach

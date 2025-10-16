b93568da0e19b88ccaeaf14f86370b8e470da684
summbal122 <sunbalijaz13@gmail.com>
2025-10-16 10:28:57 +0500

    Add code

--- linked-list/designLinkedList.js
+++ linked-list/designLinkedList.js
@@ -6,3 +6,46 @@ function Node(val){
   this.val=val;
   this.next=null;
 }
+function addAtHead(val){
+  let newNode = new Node(val)
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size = this.size +1
+}
+
+function addAtTail(val){
+    let newNode = new Node(val)
+  if (this.head === null){
+  
+    this.head = newNode;
+    
+  } else {
+      let curr = this.head;
+  while(curr.next !== null){
+    curr = curr.next;
+  }
+  curr.next = newNode;
+
+  }
+}
+
+function addAtIndex(index, val){
+  let newNode = new Node(val);
+
+  if (index === 0){ //add at first index
+     this.addAtHead(val)
+     return
+  } else if (index ===this.size){ //add at last index
+    this.addAtTail(val)
+    return
+  } else {
+      let curr = this.head;
+  for(let i = 0; i < index - 1 ; i ++){
+    curr = curr.next
+  }
+  newNode.next = curr.next; // don't change order
+  curr.next = newNode;
+
+  }
+
+}

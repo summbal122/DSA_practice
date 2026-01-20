cf21e149cb7581b70c827b55d6b340aeba938ee6
summbal122 <sunbalijaz13@gmail.com>
2026-01-20 07:53:10 +0500

    Add code

---
+++ linked-list/removeDuplicatesSortedList.js
@@ -0,0 +1,12 @@
+function removeDuplicates(head){
+  let curr = head;
+
+  while (curr && curr.next){
+    if(curr.val === curr.next.val){
+      curr.next = curr.next.next
+    } else {
+      curr = curr.next
+    }
+  }
+  return head;
+}
